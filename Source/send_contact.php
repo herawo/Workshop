8364e865366da5002cd75ab001a4aab2ec54f891
Clemalex <clemalex20@gmail.com>
2017-10-04 11:33:23 +0200

    del of redondant files

--- Source/send_contact.php
+++ Source/send_contact.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 $user = 'root';
 $pass = '';
 $dbh = new PDO('mysql:host=localhost;dbname=assist_com', $user, $pass);
@@ -17,6 +18,7 @@ $file=$_POST['file'];
 
 
 $stmt = $dbh->prepare("INSERT INTO projet(date, contact_mail, full_description, contact_name, title, duration_month, start, location, rate, file)  VALUES (:date, :contact_mail, :full_description, :contact_name, :title, :duration_month, :start, :location, :rate, :file)");
+
 $stmt->bindParam(':date', $date);
 $stmt->bindParam(':contact_mail', $contact_mail);
 $stmt->bindParam(':full_description', $full_description);
